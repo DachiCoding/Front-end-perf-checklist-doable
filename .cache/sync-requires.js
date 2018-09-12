c3b2dcdfadda4f6621ddc7f223feed2c7221c15f
xudachi <xudachi@bytedance.com>
2018-09-12 15:10:05 +0800

    update gitignore

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,7 +7,6 @@ exports.layouts = {
 }
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/xudachi/Documents/workspace/projects/front-end-perf-checklist/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/xudachi/Documents/workspace/projects/front-end-perf-checklist/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/xudachi/Documents/workspace/projects/front-end-perf-checklist/src/pages/index.js")),
   "component---src-pages-page-2-js": preferDefault(require("/Users/xudachi/Documents/workspace/projects/front-end-perf-checklist/src/pages/page-2.js"))
@@ -15,7 +14,6 @@ exports.components = {
 
 exports.json = {
   "layout-index.json": require("/Users/xudachi/Documents/workspace/projects/front-end-perf-checklist/.cache/json/layout-index.json"),
-  "dev-404-page.json": require("/Users/xudachi/Documents/workspace/projects/front-end-perf-checklist/.cache/json/dev-404-page.json"),
   "404.json": require("/Users/xudachi/Documents/workspace/projects/front-end-perf-checklist/.cache/json/404.json"),
   "index.json": require("/Users/xudachi/Documents/workspace/projects/front-end-perf-checklist/.cache/json/index.json"),
   "page-2.json": require("/Users/xudachi/Documents/workspace/projects/front-end-perf-checklist/.cache/json/page-2.json"),
